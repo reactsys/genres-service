66ab3ff7a842321864b69bd59c13905a2cda9d76
paulb150 <paul.broekman@gmail.com>
2018-03-07 09:19:04 +0200

    Add server CRUD functionality

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 
 const app = express();
 
@@ -22,16 +23,47 @@ app.get("/api/genres/:id",(req,res)=>{
 });
 
 app.post("/api/genres",(req,res)=>{
+    let result = validateGenre(req.body);
+    if(result.error) return res.status(400).send(result.error);
 
+    const genre = { 
+        id: genres.length + 1,
+        name: req.body.name
+    };
+
+    genres.push(genre);
+    res.send(genre);
 });
 
 app.put("/api/genres/:id",(req,res)=>{
+    
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("Genre with given id not found");
+    
+    let result = validateGenre(genre);
+    if(result.error) return res.status(400).send(result.error);
 
+    genre.name = req.body.name;
+    res.send(genre);
 });
 
 app.delete("/api/genres/:id",(req,res)=>{
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("Genre with given id not found");
 
+    const index = genres.indexOf(genre);
+    genres.splice(index);
+
+    res.send(genre);
 });
+
+function validateGenre(genre){
+    const schema = {
+        name: Joi.string().max(50).required()
+    }
+
+     return Joi.validate(genre, schema);
+}
 
 let port = 4000;
 app.listen(port,()=>{
